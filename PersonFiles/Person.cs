152a38f2c7d2f911f114a3d966c280d0b33a2c28
choptankw <91840361+choptankw@users.noreply.github.com>
2024-09-20 19:28:30 +0400

    Add project files.

---
+++ PersonFiles/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonFiles
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+
+        public Person(string name, string surname, byte age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, Surname = {Surname}, Age = {Age}";
+        }
+    }
+}
---
+++ PersonFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace PersonFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person("John", "Doe", 25));
+            people.Add(new Person("Jane", "Doe", 20));
+
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/people.json";
+
+            string peopleJson = JsonSerializer.Serialize(people);
+
+            File.WriteAllText(filePath, peopleJson);
+
+            /////
+
+            string json = File.ReadAllText(filePath);
+
+            people = JsonSerializer.Deserialize<List<Person>>(json);
+
+            foreach (var p in people)  
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
