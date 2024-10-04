898619fa43785e9e759d85e4b5d612aa245f3ad8
choptankw <91840361+choptankw@users.noreply.github.com>
2024-10-04 21:15:21 +0400

    Reflection added

---
+++ Reflection/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    private int _id;
+    public byte Age { get; set; }
+
+    public Person() { }
+    public Person(string name, string surname, int id, byte age)
+    {
+        Name = name;
+        Surname = surname;
+        _id = id;
+        Age = age;
+    }
+
+    public void PrintPersonInfo()
+    {
+        Console.WriteLine($"Full Name - {Name} {Surname}, {Age} years old, ID - {GetId()}");
+    }
+
+    private int GetId()
+    {
+        return _id;
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Dynamic;
+using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type classType = typeof(Person);
+
+            PropertyInfo[] properties = classType.GetProperties();
+            FieldInfo[] fileds = classType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            MethodInfo[] methods = classType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var instanceOfClass = Activator.CreateInstance(classType);
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.Name == "Name")
+                    property.SetValue(instanceOfClass, "Chuck");
+                if (property.Name == "Surname")
+                    property.SetValue(instanceOfClass, "Norris");
+                if (property.Name == "Age")
+                    property.SetValue(instanceOfClass, (byte)65);
+            }
+
+            foreach (FieldInfo field in fileds)
+            {
+                if (field.Name == "_id")
+                    field.SetValue(instanceOfClass, 234535);
+            }
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.Name == "PrintPersonInfo")
+                    method.Invoke(instanceOfClass, null);
+                if (method.Name == "GetId")
+                    method.Invoke(instanceOfClass, null);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
